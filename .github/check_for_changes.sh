6e0da8847d1a71868121fd8f5d4d8cfb448c0198
Dave Verwer <dave.verwer@gmail.com>
2026-03-20 09:12:01 +0000

    Ignore whitespace when checking for changes.

--- .github/check_for_changes.sh
+++ .github/check_for_changes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if diff -q blogs.json new_blogs.json > /dev/null 2>&1; then
+if diff -q -w blogs.json new_blogs.json > /dev/null 2>&1; then
     echo "changes=false" >> "$GITHUB_OUTPUT"
 else
     echo "changes=true" >> "$GITHUB_OUTPUT"
